d00455487e8a327b44948fcf340d822654630692
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-01-22 11:57:12 +0200

    Shopping list excercise

---
+++ Shopping list/js/script.js
@@ -0,0 +1,27 @@
+var array = [];
+
+
+
+function addToList(){
+	var product = document.getElementById("product").value;	
+	console.log('pridedam:' + product);	
+				
+	//irasit nauja produkta i masiva
+	array.push(product);
+	//console.log(array);
+	//atvaizduot rezultata.	
+	//printArray(productList);
+	renderArray(array);
+}
+
+function renderArray(x){
+	//console.log(x);
+	var h = "";
+	h = "<ul>";
+	for(var i = 0;i < x.length;i++){
+		console.log(x[i]);
+		h = h + "<li>" +x[i]+"</li>";
+	}
+	h = h + "</ul>"
+	document.getElementById("result").innerHTML = h;
+}
